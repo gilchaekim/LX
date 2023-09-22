92ba4840c905a724d7c29cb5adb668ec8ac9c52f
gilchaekim <kk1230t@gmail.com>
2023-09-22 17:40:02 +0900

    124

--- src/asset/js/components/index.js
+++ src/asset/js/components/index.js
@@ -8,6 +8,7 @@ export {default as Tree} from './core/tree';
 export {default as Addimage} from './core/addimage';
 export {default as Acclist} from './core/acclist';
 export {default as Scrollbar} from './core/scrollbar';
+export {default as Datepicker} from './core/datepicker';
 
 // 마크업진행표
 export {default as Worklists} from './custom/worklists';
